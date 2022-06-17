7bbe3d6ac5b54da8493aecaef22e08fb8b450c8f
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-06-17 16:42:59 +0300

    Assignment Complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (event) =>{
+        event.preventDefault()
+        // console.log(event.target.children[1])
+        const input = document.querySelector('input#searchByID')
+        // console.log(input.value)
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(resp => resp.json())
+        .then(data => {
+            title = document.querySelector('section#movieDetails h4')
+            summary = document.querySelector('section#movieDetails p')
+    
+            title.textContent = data.title
+            summary.textContent = data.summary
+    
+        })
+
+
+    }) 
 }
 
 document.addEventListener('DOMContentLoaded', init);
